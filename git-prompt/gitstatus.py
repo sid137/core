c359b8c0f3578fc2f21fd091068fc4d942e9ef56
Sidney Burks <sid137@gmail.com>
2014-03-13 22:49:44 +0100

    more prompt changes

--- git-prompt/gitstatus.py
+++ git-prompt/gitstatus.py
@@ -14,8 +14,8 @@ if error:
 lines = output.splitlines()
 
 import re
-behead_re = re.compile(r"^# Your branch is (ahead of|behind) '(.*)' by (\d+) commit")
-diverge_re = re.compile(r"^# and have (\d+) and (\d+) different")
+behead_re = re.compile(r"^Your branch is (ahead of|behind) '(.*)' by (\d+) commit")
+diverge_re = re.compile(r"^and have (\d+) and (\d+) different")
 
 status = ''
 staged = re.compile(r'^Changes to be committed:$', re.MULTILINE)
